80503e1395c12734df919a73a489343bb51cd793
Johannes Siebel <Johannes.Siebel@leanix.net>
2022-07-18 22:19:55 +0200

    Set up simple test to reproduce problems with Rancher Desktop on M1

--- src/test/java/org/testcontainers/repro/ReproExampleTest.java
+++ src/test/java/org/testcontainers/repro/ReproExampleTest.java
@@ -1,9 +1,21 @@
 package org.testcontainers.repro;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
+
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import javax.sql.DataSource;
 import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.testcontainers.containers.GenericContainer;
+import org.testcontainers.containers.JdbcDatabaseContainer;
+import org.testcontainers.containers.PostgreSQLContainer;
 import org.testcontainers.utility.DockerImageName;
 
 public class ReproExampleTest {
@@ -19,15 +31,38 @@ public class ReproExampleTest {
      * description.
      */
     @Test
-    public void demonstration() {
+    public void demonstration() throws SQLException {
         try (
-            // customize the creation of a container as required
-            GenericContainer<?> container = new GenericContainer<>(DockerImageName.parse("redis:6.0.5"))
-                    .withExposedPorts(6379)
+            PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>("postgres:11")
+                .withDatabaseName("DB_NAME")
+                .withUsername("USER")
+                .withPassword("PWD")
         ) {
-            container.start();
+            postgres.start();
 
-            // ...
+            ResultSet resultSet = performQuery(postgres, "SELECT 1");
+
+            int resultSetInt = resultSet.getInt(1);
+            assertEquals("A basic SELECT query succeeds", 1, resultSetInt);
         }
+    }
+
+    protected ResultSet performQuery(JdbcDatabaseContainer<?> container, String sql) throws SQLException {
+        DataSource ds = getDataSource(container);
+        Statement statement = ds.getConnection().createStatement();
+        statement.execute(sql);
+        ResultSet resultSet = statement.getResultSet();
+
+        resultSet.next();
+        return resultSet;
+    }
+
+    protected DataSource getDataSource(JdbcDatabaseContainer<?> container) {
+        HikariConfig hikariConfig = new HikariConfig();
+        hikariConfig.setJdbcUrl(container.getJdbcUrl());
+        hikariConfig.setUsername(container.getUsername());
+        hikariConfig.setPassword(container.getPassword());
+        hikariConfig.setDriverClassName(container.getDriverClassName());
+        return new HikariDataSource(hikariConfig);
     }
 }
